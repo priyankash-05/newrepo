510ef6a45cbf0df286d265d4e3c7a9cbb86a7c98
priyankash-05 <p.harakari@gmail.com>
2021-04-15 16:14:25 +0530

    fw new changes

---
+++ src/newdemo1/Demotest.java
@@ -0,0 +1,11 @@
+package newdemo1;
+
+public class Demotest {
+
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+	}
+
+}
